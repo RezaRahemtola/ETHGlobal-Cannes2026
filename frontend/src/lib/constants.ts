3f55baf3af4ec885a269cceff44e89fda4e2c0a0
Reza Rahemtola <reza.rahemtola@epitech.eu>
2026-04-04 01:58:10 +0200

    fix: update action to humanens, add WORLD_RP_ID constant

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -3,4 +3,5 @@ export const HUMANENS_LINKER_ADDRESS = process.env.NEXT_PUBLIC_LINKER_ADDRESS as
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 
 export const WORLD_APP_ID = process.env.NEXT_PUBLIC_WORLD_APP_ID || "";
-export const WORLD_ACTION_ID = process.env.NEXT_PUBLIC_WORLD_ACTION_ID || "register-humanens";
+export const WORLD_ACTION_ID = process.env.NEXT_PUBLIC_WORLD_ACTION_ID || "humanens";
+export const WORLD_RP_ID = process.env.NEXT_PUBLIC_WORLD_RP_ID || "";
