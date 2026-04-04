2defc7f443431c480f742b11a3625338984b3ce0
Reza Rahemtola <reza.rahemtola@epitech.eu>
2026-04-05 01:37:51 +0200

    feat(front): Remove Eruda

--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -5,25 +5,18 @@ import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
 import { WagmiProvider } from "wagmi";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { config } from "@/lib/wagmi-config";
-import dynamic from "next/dynamic";
 import { useState, type ReactNode } from "react";
-
-const ErudaProvider = dynamic(() => import("@/providers/Eruda").then((c) => c.ErudaProvider), {
-  ssr: false,
-});
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <ErudaProvider>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <MiniKitProvider>
-            <NuqsAdapter>{children}</NuqsAdapter>
-          </MiniKitProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </ErudaProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <MiniKitProvider>
+          <NuqsAdapter>{children}</NuqsAdapter>
+        </MiniKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
